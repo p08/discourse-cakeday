d0bd96d206d3877ee8283f20233a07fb51307622
P <prasen@rathore.com.au>
2021-05-28 21:54:23 +0930

    Update 20200117010102_add_top_topics_msecs_columns.rb

--- db/migrate/20200117010102_add_top_topics_msecs_columns.rb
+++ db/migrate/20200117010102_add_top_topics_msecs_columns.rb
@@ -1,7 +1,7 @@
 class AddTopTopicsMsecsColumns < ActiveRecord::Migration[5.2]
   def change
     [:daily, :weekly, :monthly, :quarterly, :yearly].each do |period|
-        add_column :top_topics, "#{period}_msecs_count".to_sym, :integer, null: false, default: 0
+        remove_column :top_topics, "#{period}_msecs_count".to_sym, :integer, null: false, default: 0
     end
   end
 end
